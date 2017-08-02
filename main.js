df3b1e589bededaeffcd3d0589e05803631512d9
Grant McGrath <grantmcgrath@me.com>
2017-08-02 19:48:42 -0400

    Completed assignment. Quite happy with the final results.

--- main.js
+++ main.js
@@ -15,9 +15,25 @@ app.set("view engine", "mustache");
 app.use(express.static("public"));
 
 // Configures Webroot
-app.get("/", function(req, res){
-  res.render("home", data)
+app.get("/", function(request, response){
+  response.render("home", data)
 });
+
+// Something I tried/I couldn't figure this out.
+// app.get("/card/:user", function(request, response) {
+//   response.render("card", {
+//     card: data[request.params.user]
+//   });
+// });
+
+app.get("/:user", function(req, res) {
+      let user = req.params.user;
+      for (i = 0; i < data.users.length; i++) {
+        if (data.users[i].username === user) {
+          res.render("card", data.users[i])
+        }
+      }
+    });
 
 // Tell js to listen to port 3000
  app.listen(3000, function() {
